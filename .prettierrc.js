16641be4a99976ce5d2daf59a3b69615e46836aa
Stephan Müller <stephan.mueller.w@gmail.com>
2020-01-28 13:43:11 +0100

    Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // https://github.com/danreeves/sublime-prettier#configuration
+  autoformat: true, // save on exit
+  printWidth: 500,
+  extensions: ["js"],
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  arrowParens: "avoid",
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: false,
+  trailingComma: "all",
+  breakBeforeElse: true
+};
